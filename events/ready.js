d972ef0d00c1407f9d4f26c88400c992b86ce1b0
Greensky-gs <david.hesliere@gmail.com>
2022-07-09 18:09:39 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const { Client, MessageEmbed } = require('discord.js');
-const { connect, beta, statsYeikzy } = require('../assets/data/data.json');
+const { connect, beta, statsYeikzy, default_prefix } = require('../assets/data/data.json');
 
 module.exports = {
     event: 'ready',
@@ -26,11 +26,6 @@ module.exports = {
 
         slashCommandsBuilder();
         setInterval(slashCommandsBuilder, 600000);
-
-        client.user.setActivity({
-            name: 'la version ' + require('../assets/data/data.json').version,
-            type: 'WATCHING',
-        });
 
         const dbl = require('dblapi.js');
         client.dbl = new dbl(require('../assets/data/data.json').token, {
@@ -71,6 +66,23 @@ module.exports = {
     
                 web.send({ embeds: [ embed ] }).catch(() => {});
             })
-        }
+        };
+
+        let statusIndex = 0;
+        let status = [
+            {name: 'la version ' + require('../assets/data/data.json').version, type: 'WATCHING'},
+            {name: `avec ${client.users.cache.size} utilisateurs`, type: 'PLAYING'},
+            {name: `${client.guilds.cache.size} serveurs`, type: 'STREAMING'},
+            {name: `un message qui commence par ${default_prefix}`, type: 'LISTENING'}
+        ];
+        
+        setInterval(() => {
+            statusIndex++;
+            if (statusIndex > status.length) statusIndex = 0;
+
+            let statut = status[statusIndex];
+
+            client.user.setActivity(statut);
+        }, 20000);
     }
 };
